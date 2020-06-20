54b389a924c80f1164e735dc378ca3f0dd103b23
Adam <adt.mail1@gmail.com>
2020-06-20 16:28:36 +0200

    commit first items code

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/adataou/bookstore_items-api/app"
+)
+
+
+func main()  {
+	//os.Setenv("LOG_LEVEL", "info")
+	app.StartApplication()
+}
+
+
---
+++ src/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/adataou/bookstore_items-api/src/application"
+)
+
+func main() {
+	application.StartApplication()
+}
